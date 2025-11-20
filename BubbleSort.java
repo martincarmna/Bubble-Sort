61fef672494c380e65c82b299b4e66bd621f91ea
martincarmna <mcarmonapalestina@gmail.com>
2025-11-19 21:57:40 -0600

    creacion de buble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -26,3 +26,20 @@ public class BubbleSort {
             System.out.println("Error: " + e.getMessage());
         }
     }
+    // ---- Bubble Sort ----
+    public static void bubbleSort(int[] arr) {
+        boolean swapped;
+        int n = arr.length;
+
+        do {
+            swapped = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+    }
